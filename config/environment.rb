f9f433e66f612e5db954d586c3e9aee4efb32a46
Abrahan Martinez <amm172005@gmail.com>
2021-10-12 09:31:17 -0500

    Finished Devise email confirmation step. Used SendGrid API.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+  :password => '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', # This is the secret sendgrid API key which was issued during API key creation
+  :domain => 'gmail.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
