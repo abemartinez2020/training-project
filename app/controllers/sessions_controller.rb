7c542048e6e22e76cc5b4acb1494384f8af4cd2f
Abrahan Martinez <amm172005@gmail.com>
2021-10-15 11:28:58 -0500

    deleted extra files

--- app/controllers/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-  protected
-
-  def after_inactive_sign_up_path_for(resource)
-    new_customer_confirmation_path
-  end
-
-end
-
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,12 +1,4 @@
 class Profile < ApplicationRecord
   
-  def new
-    @profile = Profile.new
-  end
-
-  def create
-    
-  end
-
 end
 
