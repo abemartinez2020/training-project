626a3186da92f4bcf5312b659000bc925ee109ad
Abrahan Martinez <amm172005@gmail.com>
2021-10-15 10:38:06 -0500

    Finally managed to redirect to a profile page for first-time sign ins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
   protected
 
+  def after_sign_in_path_for(resource)
+    if resource.sign_in_count == 1
+      profiles_new_path
+    else
+      root_path
+    end
+  end
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
